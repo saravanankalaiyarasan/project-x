1b4afae393468a4dd342566e9962f841427a2801
saravanankalaiyarasan <saracivil1995@gmail.com>
2022-03-16 01:20:55 +0530

    new code 2

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -1,14 +1,77 @@
 package org.test;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.test.base.Base;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Sample {
+public class Sample extends Base{
+	
+	@BeforeClass
+	private void beforeClass() {
+		browserLaunch();
+		urlLaunch("https://www.flipkart.com/");
+	}
 	
 	@Test
 	private void test1() {
-		System.out.println("654161");
-		System.out.println("saravanan kalaiayarasan");
+		
+		driver.findElement(By.xpath("//button[@class='_2KpZ6l _2doB4z']")).click();
+		driver.findElement(By.name("q")).sendKeys("iphone");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		
 	
+	}
+	@Test
+	private void test2() {
+		List<WebElement> list = driver.findElements(By.xpath("//div[@class='_4rR01T']"));
+		
+		for(int i=0;i<list.size();i++) {
+		WebElement a = list.get(i);
+		String text = a.getText();
+		System.out.println(text);
+		}
+	}
+	
+	@Test
+	private void test3() {
+		List<WebElement> list = driver.findElements(By.xpath("//div[@class='_30jeq3 _1_WHN1']"));
+		 String y="";
+		 
+		for(int i=0;i<list.size();i++) {
+		WebElement a = list.get(i);
+		String text = a.getText();
+		
+		String x="";
+		//String y="";
+			if(text.contains("₹")) {
+				x=text.replace("₹", "");
+		
+			}
+			if(x.contains(",")) {
+			y=x.replace(",", "");
+			System.out.println(y);
+
+			
+			}
+			int z=Integer.parseInt(y);
+			
+			List<Integer> li=new ArrayList<Integer>();
+			li.add(z);
+			Collections.sort(li);
+			System.out.println(li);
+
+			System.out.println(li.get(0));
+			System.out.println(li.get(li.size()-1));
+		}
+		
 	}
 
 }
